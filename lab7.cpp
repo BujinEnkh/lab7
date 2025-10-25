033d35e77c277e5e24351eebfe51963a7e556c5a
aka3058 <esenser123@gmail.com>
2025-10-25 22:51:13 +0800

    Initial commit

---
+++ lab7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+    double gpa;
+
+public:
+    Student(string n, int a, double g) {
+        name = n;
+        age = a;
+        gpa = g;
+        cout << "Student object created! linkers" << endl;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "GPA: " << gpa << endl;
+    }
+};
+
+int main() {
+    //Обьект үүсгэх үед байгуулагч функц автоматаар дуудагдана
+    Student s1("Bat", 20, 3.8);
+    
+    s1.display(); // Мэдээлэл хэвлэх
+    return 0;
+}
